e72a0aef8bb5129d7b84d7924aae1325b4df876b
administrator <liquidnuker@users.noreply.github.com>
2018-01-27 11:05:17 -0800

    replaced checkbox onClick with onChange

--- src/components/AuthFilter.jsx
+++ src/components/AuthFilter.jsx
@@ -7,7 +7,7 @@ export default class AuthFilter extends React.Component {
       {this.props.pr_items.map((i, index) =>
         <li>
         <input type="checkbox" value={i.authName} checked={i.checked} 
-        onClick={() => { this.props.pr_val_toggleAuthType(index)}} />
+        onChange={() => { this.props.pr_val_toggleAuthType(index)}} />
         <label tabIndex="0">{i.authName ? i.authName : 'null'}</label>
         </li>
       )}
--- src/components/HttpsToggle.jsx
+++ src/components/HttpsToggle.jsx
@@ -14,7 +14,7 @@ export default class HttpsToggle extends React.Component {
     return (
       <div className="authfilter_https--toggle">
       <input type="checkbox" id="checkbox" checked={this.props.pr_https} 
-      onClick={() => { this.props.pr_val_toggleHttps(this.props.pr_https)}}/>
+      onChange={() => { this.props.pr_val_toggleHttps(this.props.pr_https)}}/>
       <label tabIndex="0" htmlFor="checkbox">HTTPS only</label>
       </div>
     );
